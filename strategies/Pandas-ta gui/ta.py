c19c4b2e72246534624a65c497b6853ea81051aa
smidolt <smidolty@gmail.com>
2024-02-08 08:52:28 +0100

    v119

--- strategies/Pandas-ta gui/ta.py
+++ strategies/Pandas-ta gui/ta.py
@@ -5,8 +5,12 @@ import ccxt, yfinance
 
 print(ccxt.exchanges) # print a list of all available exchange classes
 
-exchange = ccxt. binance()
+exchange = ccxt.binance()
 bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='5m', limit=500)
 df = pd.DataFrame(bars, columns=['time','open','high','low','close','volume'])
-
+ema = ta.ema(close= df['close'], length=200)
+print(ema)
 print(df)
+df = df.ta.ema(close= df['close'], length=200)
+print(df)
+#df.to_csv('Biodata.csv', index=False)
