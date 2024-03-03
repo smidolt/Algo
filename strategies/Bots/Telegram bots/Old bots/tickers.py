3e732bb9ac65e89ecb2427c7c0f8f3a602027b95
smidolt <smidolty@gmail.com>
2024-03-03 03:51:49 +0100

    V1.4

---
+++ strategies/Bots/Telegram bots/Old bots/tickers.py
@@ -0,0 +1,20 @@
+tickers = [
+    'BTC', 'ETH', 'BNB', 'BUSD', 'ADA', 'DOT', 'XRP', 'UNI', 'LTC', 'LINK',
+    'BCH', 'VET', 'TRX', 'EOS', 'FTT', 'THETA', 'ATOM', 'XTZ', 'XLM', 'NEO',
+    'FIL', 'DOGE', 'ALGO', 'QTUM', 'IOTA', 'ZEC', 'KSM', 'WAVES', 'MKR',
+    'COMP', 'OMG', 'SUSHI', 'LRC', 'BAT', 'ZRX', 'SNX', 'CRV', 'ICX',
+    'EGLD', 'KAVA', 'ZIL', 'BCHABC', 'BNT', 'MANA', 'GRT', 'NMR', '1INCH',
+    'REN', 'OXT', 'HNT', 'FLM', 'UNFI', 'FRONT', 'ALICE', 'LUNA', 'LIT',
+    'UTK', 'XVS', 'CAKE', 'BAND', 'BZRX', 'SXP', 'CTK', 'TOMO', 'REEF',
+    'OG', 'CREAM', 'RSR', 'MATIC', 'AKRO', 'BURGER', 'BETH', 'FIS', 'DODO',
+    'IOTX', 'BEL', 'WINK', 'SRM', 'TLM', 'DGB', 'STMX', 'SAND', 'AUCTION',
+    'AXS', 'HBAR', 'ASR', 'MASK', 'UND', 'ROSE', 'AVAX', 'XEM', 'TRB',
+    'EZ', 'EPS', 'ALPHA', 'VIDT', 'CHR', 'RAY', 'FARM', 'ALPHA', 'POLS',
+    'QUICK', 'ORN', 'MINA', 'RAY', 'BAKE', 'AR', 'POND', 'DODO', 'ZEN',
+    'CFX', 'TOMOE', 'GTC', 'CTK', 'PUNDIX', 'MDX', 'MIR', 'LIT', 'UTK',
+    'XVS', 'LUNA', 'RSR', 'FRONT', 'ALICE', 'LINA', 'DGB', 'STMX', 'SAND',
+    'AUCTION', 'AXS', 'HBAR', 'ASR', 'MASK', 'XEM', 'TRB'
+]
+
+tickers_str = '\n'.join(tickers)
+
